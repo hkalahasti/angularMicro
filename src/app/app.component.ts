cef81addcb45f7623dfe408d70094247dedbdf92
hkalahasti <hkalahasti@evoketechnologies.com>
2018-09-17 17:05:08 +0530

    iocns registry added for bypass svg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, ViewContainerRef } from '@angular/core';
-
+import { MatIconRegistry } from '@angular/material';
+import {EShareDataService} from './e-shared/e-sharedata.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(public viewRef : ViewContainerRef) {}
+  constructor(public viewRef : ViewContainerRef,  private _iconRegistry: MatIconRegistry,private eShareDataService:EShareDataService) {
+    this.loadIcons();
+  }
+  loadIcons(){
+    this._iconRegistry.addSvgIconSetInNamespace('core',
+      this.eShareDataService.bypassURL('/assets/icons/app-icons.svg'));
+}
 }
--- src/app/e-shared/e-sharedata.service.ts
+++ src/app/e-shared/e-sharedata.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import {Storage} from './utils/storage';
+import { MatSnackBar} from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+
 @Injectable()
 export class EShareDataService {
 
   private messageSource = new BehaviorSubject('default message');
   currentMessage = this.messageSource.asObservable();
 
-  constructor() { }
+  constructor(private _domSanitizer: DomSanitizer, private _snackBarService: MatSnackBar ) { }
 
   changeMessage(message: string) {
     this.messageSource.next(message)
@@ -25,5 +28,11 @@ export class EShareDataService {
   removeKey(key) {
      Storage.removeSessionItem(key);
    }
+   showMessage(message: string){
+    this._snackBarService.open(message, 'x',{ duration: 3000 });
+}
+   bypassURL(url: string){
+    return this._domSanitizer.bypassSecurityTrustResourceUrl(url);
+}
 
 }
