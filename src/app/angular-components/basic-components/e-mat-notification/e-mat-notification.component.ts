795230e4725a0a526dbe0387fbd5c81738ca0167
hkalahasti <hkalahasti@evoketechnologies.com>
2018-09-21 19:55:52 +0530

    commit

--- src/app/angular-components/basic-components/e-mat-notification/e-mat-notification.component.ts
+++ src/app/angular-components/basic-components/e-mat-notification/e-mat-notification.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { Code } from './code';
+import { EShareDataService } from '../../../e-shared/e-sharedata.service'
 @Component({
   selector: 'app-e-mat-notification',
   templateUrl: './e-mat-notification.component.html',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EMatNotificationComponent implements OnInit {
   notifications: any = "101"
-  constructor() { }
-
+  
+  code: any;
+  constructor(private eShareDataService: EShareDataService) { }
+  
   ngOnInit() {
+    this.code = Code;
+    this.eShareDataService.pushCode(this.code);
+    console.log(this.code)
   }
 
 }
